29a507897692ec4a5febab9b1cb9e82b13bd56dc
iamnotstatic <staticdev20046@gmail.com>
2021-07-08 09:26:40 +0100

    init homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import useDarkMode from './hooks/useDarkMode';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 import Navbar from './components/Navbar';
 
 const App = () => {
-  const [colorTheme, setTheme] = useDarkMode();
-
   return (
-    <div className="w-full dark:bg-gray-800">
+    <div className="font-mono">
       <Navbar />
-      
+      <Tabs className="text-center mt-10 text-sm">
+        <TabList>
+          <Tab>Stake</Tab>
+          <Tab>Withdraw</Tab>
+        </TabList>
+
+        <TabPanel className="mt-5">
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
